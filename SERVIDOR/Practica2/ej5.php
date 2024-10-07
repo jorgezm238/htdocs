fbf9dd55a010f2ba6db1d42d9b5069042b8a5aa6
Jorge Zardain Monforte <wrn47806@educastur.es>
2024-10-07 13:43:24 +0200

    aa

---
+++ SERVIDOR/Practica2/ej5.php
@@ -0,0 +1,12 @@
+<?php
+
+/*Generar de forma aleatoria una matriz de 3x5 con valores numéricos.
+a. Imprimir todos los elementos en forma sucesiva tomándolos por fila.
+b. Igual al anterior pero por columna.*/
+array 
+
+
+
+
+
+?>
