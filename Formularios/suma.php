ac8f4cabcb7878bc9a49b3ea95c57ab0f28c94ec
Jorge Zardain Monforte <wrn47806@educastur.es>
2024-10-21 13:56:07 +0200

    no

--- Formularios/suma.php
+++ Formularios/suma.php
@@ -1,8 +1,8 @@
 <?php
 echo  "El resultado de la suma es ". $_POST['num1'] + $_POST['num2'];
 echo   '<br>';
-var_dump($_SERVER['HTTP_SEC_CH_UA']); 
-var_dump($_SERVER['DOCUMENT_ROOT']);         
+//var_dump($_SERVER['HTTP_SEC_CH_UA']); 
+//var_dump($_SERVER['DOCUMENT_ROOT']);         
 
 
 ?>
