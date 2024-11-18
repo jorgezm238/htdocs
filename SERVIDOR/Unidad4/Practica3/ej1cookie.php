299d22e08a7844731e6c63250e9a9e30ce6139be
jorgezm238 <wrn47806@educastur.es>
2024-11-18 15:24:52 +0100

    cookie

---
+++ SERVIDOR/Unidad4/Practica3/ej1cookie.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ejercicio2</title>
+</head>
+<body>
+   <p>Seleccione de que color desea que sea la web de ahora en adelante:</p>
+        <form action="ej1cookie.php" method="post">
+            <input type="radio" id="rojo" name="rojo">
+            <label>rojo</label>
+            <input type="radio" id="verde" name="verde">
+            <label>verde</label>
+            <input type="radio" id="azul" name="azul">
+            <label>azul</label>
+            <br>
+            <input type="submit" name="submit" value="Crear cookie">
+            <?php 
+                session_start();        
+            ?>   
+        </form>
+</body>
+</html>
